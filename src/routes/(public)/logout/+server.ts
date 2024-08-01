1e752da756a558694d0575e37d14cf272c0ec3f8
Anoop B <16367697+anoop-b@users.noreply.github.com>
2024-08-02 01:37:35 +0300

    fix: changed POST to GET

--- src/routes/(public)/logout/+server.ts
+++ src/routes/(public)/logout/+server.ts
@@ -1,6 +1,6 @@
 import { redirect, type RequestHandler } from "@sveltejs/kit"
 
-export const POST: RequestHandler = async ({ cookies }) => {
+export const GET: RequestHandler = async ({ cookies }) => {
     cookies.delete("CF_Authorization", {
         path: '/'
     })
