f3d9f88ac7a87ba06b0cc131dc48c0ccc94e9952
Anoop B <16367697+anoop-b@users.noreply.github.com>
2024-08-02 16:50:06 +0300

    refactor: open link in new tab

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -3,14 +3,14 @@
 </script>
 
 <div class="mx-auto">
-
     <h1
         class="scroll-m-20 text-5xl text-center font-extrabold tracking-tight lg:text-5xl"
     >
         An edge first feature flag implementation
     </h1>
     <div class="mt-10 flex items-center justify-center">
-
-        <Button href="/dashboard">Dashboard</Button>
+        <Button data-sveltekit-replacestate target="_blank" href="/dashboard"
+            >Dashboard</Button
+        >
     </div>
 </div>
