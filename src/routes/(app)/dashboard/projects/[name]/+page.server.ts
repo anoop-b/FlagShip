5d39ad7faefec98acc12c7dcf5d487f08724bcec
Anoop B <16367697+anoop-b@users.noreply.github.com>
2024-08-17 20:29:51 +0300

    feat: init /project/[name] endpoint

---
+++ src/routes/(app)/dashboard/projects/[name]/+page.server.ts
@@ -0,0 +1,20 @@
+import { drizzle } from "drizzle-orm/d1";
+import type { PageServerLoad } from "./$types";
+import * as schema from "$lib/drizzle/schema";
+import { eq } from 'drizzle-orm';
+
+export const load = (async (events) => {
+    const name = events.params.name;
+
+  const db = drizzle(events.platform?.env.DB!, { schema });
+  const results = await db.query.projectsTable.findMany({
+    where: eq(schema.projectsTable.name, name),
+    with: {
+        flags: true,
+    },
+  });
+
+    return {
+        data: results
+    };
+}) satisfies PageServerLoad;
---
+++ src/routes/(app)/dashboard/projects/[name]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import type { PageServerData } from "./$types";
+
+    export let data: PageServerData;
+</script>
+<div>
+    <p>{data.data[0].name}</p>
+    <h1>{data.data[0].createdAt}</h1>
+    <h1 class="text-2xl font-bold">{data.data[0].updatedAt}</h1>
+</div>
+
