3423cc8fea83793e2ac86a10c09348a03d68c6dc
Anoop B <16367697+anoop-b@users.noreply.github.com>
2024-08-17 20:27:41 +0300

    feat: /dashboard UI & server side update

---
+++ src/routes/(app)/dashboard/+page.server.ts
@@ -0,0 +1,20 @@
+import { drizzle } from "drizzle-orm/d1";
+import type { PageServerLoad } from "./$types";
+import * as schema from "$lib/drizzle/schema";
+import { count } from "drizzle-orm";
+
+export const load = (async (events) => {
+  const db = drizzle(events.platform!.env.DB, { schema });
+  const projects = await db
+    .select({ count: count() })
+    .from(schema.projectsTable);
+  const flags = await db.select({ count: count() }).from(schema.flagsTable);
+
+  console.log(projects, flags);
+  return {
+    data: {
+      projects: projects,
+      flags: flags,
+    },
+  };
+}) satisfies PageServerLoad;
--- src/routes/(app)/dashboard/+page.svelte
+++ src/routes/(app)/dashboard/+page.svelte
@@ -1,19 +1,58 @@
 <script lang="ts">
-  import Button from "$lib/components/ui/button/button.svelte";
+  import * as Card from "$lib/components/ui/card";
+  import { DollarSign, Users, CreditCard, Activity } from "lucide-svelte";
+  export let data;
 </script>
 
-<div class="flex items-center">
-  <div
-    class="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-  >
-    <div class="flex flex-col items-center gap-1 text-center">
-      <h3 class="text-2xl font-bold tracking-tight">
-        Welcome to your Dashboard
-      </h3>
-      <p class="text-muted-foreground text-sm">
-        create a new project to add Flags
-      </p>
-      <Button class="mt-4">Create Project</Button>
-    </div>
+<div class="flex flex-col items-center gap-1 text-center">
+  <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+    <Card.Root>
+      <Card.Header
+        class="flex flex-row items-center justify-between space-y-0 pb-2"
+      >
+        <Card.Title class="text-sm font-medium">Total Projects</Card.Title>
+        <DollarSign class="text-muted-foreground h-4 w-4" />
+      </Card.Header>
+      <Card.Content>
+        <div class="text-2xl font-bold">{data.data.projects[0].count}</div>
+        <p class="text-muted-foreground text-xs">+20.1% from last month</p>
+      </Card.Content>
+    </Card.Root>
+    <Card.Root>
+      <Card.Header
+        class="flex flex-row items-center justify-between space-y-0 pb-2"
+      >
+        <Card.Title class="text-sm font-medium">Flags</Card.Title>
+        <Users class="text-muted-foreground h-4 w-4" />
+      </Card.Header>
+      <Card.Content>
+        <div class="text-2xl font-bold">{data.data.flags[0].count}</div>
+        <p class="text-muted-foreground text-xs">+180.1% from last month</p>
+      </Card.Content>
+    </Card.Root>
+    <Card.Root>
+      <Card.Header
+        class="flex flex-row items-center justify-between space-y-0 pb-2"
+      >
+        <Card.Title class="text-sm font-medium">Sales</Card.Title>
+        <CreditCard class="text-muted-foreground h-4 w-4" />
+      </Card.Header>
+      <Card.Content>
+        <div class="text-2xl font-bold">+12,234</div>
+        <p class="text-muted-foreground text-xs">+19% from last month</p>
+      </Card.Content>
+    </Card.Root>
+    <Card.Root>
+      <Card.Header
+        class="flex flex-row items-center justify-between space-y-0 pb-2"
+      >
+        <Card.Title class="text-sm font-medium">Active Now</Card.Title>
+        <Activity class="text-muted-foreground h-4 w-4" />
+      </Card.Header>
+      <Card.Content>
+        <div class="text-2xl font-bold">+573</div>
+        <p class="text-muted-foreground text-xs">+201 since last hour</p>
+      </Card.Content>
+    </Card.Root>
   </div>
 </div>
