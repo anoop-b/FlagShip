fe9ce58909708f48f6004a125e95ea59aaa2bb0c
Anoop B <16367697+anoop-b@users.noreply.github.com>
2024-08-17 20:29:13 +0300

    feat: /projects UI & server side updates

---
+++ src/routes/(app)/dashboard/projects/+page.server.ts
@@ -0,0 +1,27 @@
+import { drizzle } from "drizzle-orm/d1";
+import type { PageServerLoad } from "./$types";
+import * as schema from "$lib/drizzle/schema";
+import { redirect } from "@sveltejs/kit";
+
+export const load = (async (events) => {
+  const db = drizzle(events.platform?.env.DB!, { schema });
+  const results = await db.query.projectsTable.findMany({
+    with: {
+      flags: true,
+    },
+  });
+
+  if (results.length == 0) {
+    console.log("No projects found, creating new default project");
+    await db
+      .insert(schema.projectsTable)
+      .values({ name: "Default", description: "default project" })
+      .execute();
+    throw redirect(303, "/dashboard/projects");
+  }
+
+
+  return {
+    data: results,
+  };
+}) satisfies PageServerLoad;
--- src/routes/(app)/dashboard/projects/+page.svelte
+++ src/routes/(app)/dashboard/projects/+page.svelte
@@ -1,15 +1,27 @@
 <script lang="ts">
-    import Button from "$lib/components/ui/button/button.svelte";
+  import { goto } from "$app/navigation";
+  import Button from "$lib/components/ui/button/button.svelte";
+  import * as Card from "$lib/components/ui/card";
+  import { Separator } from "$lib/components/ui/separator/index.js";
+    import type { PageServerData } from "./$types";
+  export let data: PageServerData;
 </script>
-<div class="flex items-center">
-    <h1 class="text-lg font-semibold md:text-2xl">Projects</h1>
+
+<div class="flex flex-col gap-1 text-center">
+  <div class="grid gap-4 p-4 md:grid-cols-2 lg:grid-cols-4">
+    {#each data.data as project}
+      <Card.Root on:click={() => goto(`/dashboard/projects/${project.name}`)}>
+        <Card.Header class="flex pb-2">
+          <Card.Title class="text-2xl font-bold">{project.name}</Card.Title>
+          <Separator />
+        </Card.Header>
+        <Card.Content class="flex space-x-4 justify-center">
+          <p>Flags</p>
+          <Separator orientation="vertical" />
+          <div>{project.flags.length}</div>
+        </Card.Content>
+      </Card.Root>
+    {/each}
   </div>
-  <div class="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
-    <div class="flex flex-col items-center gap-1 text-center">
-      <h3 class="text-2xl font-bold tracking-tight">You have no Projects</h3>
-      <p class="text-muted-foreground text-sm">
-        create a new project to add Flags 
-      </p>
-      <Button class="mt-4">Create Project</Button>
-    </div>
-  </div>
+  <Button class="mt-4">Create Project</Button>
+</div>
