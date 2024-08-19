01e1638a62b31e7150aad8547373895ffc5b97f9
Anoop B <16367697+anoop-b@users.noreply.github.com>
2024-08-19 23:32:35 +0300

    feat: link to flag navigation

--- src/routes/(app)/dashboard/flags/+page.svelte
+++ src/routes/(app)/dashboard/flags/+page.svelte
@@ -6,7 +6,7 @@
     <div class="grid gap-4 py-4 grid-cols-2">
         {#each data.data as flag}
             <div>
-                <h1 class="text-xl">{flag.name}</h1>
+                <a class="text-xl" href="/dashboard/flags/{flag.id}">{flag.name}</a>
                 <h1>createdAt:{flag.createdAt}</h1>
             </div>
         {/each}
