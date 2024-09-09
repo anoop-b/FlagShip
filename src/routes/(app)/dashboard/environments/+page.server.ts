62c6d04940ee12ee7fafab69cee3911bc181ba62
Anoop B <16367697+anoop-b@users.noreply.github.com>
2024-09-10 00:04:39 +0300

    feat: added environments page

---
+++ src/routes/(app)/dashboard/environments/+page.server.ts
@@ -0,0 +1,19 @@
+import type { PageServerLoad } from './$types';
+import { getDb } from '$lib/server/db';
+import { error } from '@sveltejs/kit';
+
+export const load = (async (events) => {
+	const db = getDb(events);
+	// TODO: pagination
+	const results = await db.query.environmentsTable.findMany({
+		with: {
+			project: true
+		}
+	});
+	if (!results) {
+		throw error(404, 'Project Not Found');
+	}
+	return {
+		data: results
+	};
+}) satisfies PageServerLoad;
